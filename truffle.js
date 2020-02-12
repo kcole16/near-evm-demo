2be096d7e2e993641f042e9ce5659c20014d1399
Illia Polosukhin <ilblackdragon@gmail.com>
2020-02-12 12:17:30 -0800

    Adding NearProvider truffle config

--- truffle.js
+++ truffle.js
@@ -1,7 +1,18 @@
+const nearlib = require('nearlib')
+const NearProvider = require("near-web3-provider");
+
+const keyStore = new nearlib.keyStores.UnencryptedFileSystemKeyStore('neardev');
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
   networks: {
+    near: {
+        network_id: "99",
+        provider: function() {
+            return new NearProvider("http://localhost:3030", keyStore, 'test1')
+        },
+    },
     development: {
       host: "127.0.0.1",
       port: 7545,
