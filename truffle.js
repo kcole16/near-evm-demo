868c0817d717db98830397edc29b8d21495c0bab
James Prestwich <james@prestwi.ch>
2020-02-12 12:35:25 -0800

    add from to truffle

--- truffle.js
+++ truffle.js
@@ -3,6 +3,7 @@ const NearProvider = require("near-web3-provider");
 
 const keyStore = new nearlib.keyStores.UnencryptedFileSystemKeyStore('neardev');
 
+
 module.exports = {
   // See <http://truffleframework.com/docs/advanced/configuration>
   // for more about customizing your Truffle configuration!
@@ -12,11 +13,19 @@ module.exports = {
         provider: function() {
             return new NearProvider("http://localhost:3030", keyStore, 'test1')
         },
+        from: `0x${'00'.repeat(20)}`
     },
     development: {
       host: "127.0.0.1",
       port: 7545,
       network_id: "*" // Match any network id
+    }
+  },
+
+  // Configure your compilers
+  compilers: {
+    solc: {
+      version: "0.5.12",    // Fetch exact version from solc-bin (default: truffle's version)
     }
   }
 };
